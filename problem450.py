87f76b98d30ccc980d17646bebcd234d9a59eb93
Ryan Dancy <ryan@keal.ca>
2018-09-01 14:36:01 -0400

    Make problem 450 even faster; *still* not fast enough

--- problem450.py
+++ problem450.py
@@ -38,15 +38,14 @@ Find T(10^6).
 import sys
 from math import pi, cos, acos, sin, ceil, gcd
 from itertools import count
+import numpy as np
+from time import clock
 
 def is_integer(x):
   return abs(round(x) - x) < 0.00000001
 
-def is_half(x):
-  return is_integer(x + 0.5)
-
 def pimuls(R, r):
-  n = r // gcd(R, r)
+  n = r // gcd(R, r) if R % r else 1
   
   if n == 1:
     return [0]
@@ -54,7 +53,7 @@ def pimuls(R, r):
     if R % 625 == 0 and r % 250 == 0:
       return [-2, 0, 2]
     else:
-      return [-2, 0, 2]
+      return [-2, 0]
   
   if (n - 1) % 4 == 0:
     n0 = (-3*n - 1) // 2
@@ -63,7 +62,56 @@ def pimuls(R, r):
     n1 = -2*ceil(n/4)
     return [-n - (n % 2), n1, 0, -n1]
 
-def S(R, r):
+A = np.mat('1 -2 2; 2 -1 2; 2 -2 3')
+B = np.mat('1 2 2; 2 1 2; 2 2 3')
+C = np.mat('-1 2 2; -2 1 2; -2 2 3')
+
+def pythagorean_triples_up_to(limit): # all Pythagorean triples with 2c^2 < limit
+  stack = [np.mat('3; 4; 5')]
+  triples = []
+  
+  while stack:
+    current = stack.pop(0)
+    triples.append(tuple(map(int, np.squeeze(np.asarray(current)))))
+    
+    children = [A*current, B*current, C*current]
+    for child in children:
+      if 2*int(child[-1])**2 < limit:
+        stack.append(child)
+  
+  return triples
+
+def highest_powers(pythag_triples, limit): # returns dict of {c: highest exponent n with 2c^n <= limit}
+  result = {}
+  for _, _, c in pythag_triples:
+    exponent = next(i for i in count() if 2*c**(i+1) > limit)
+    result[c] = exponent
+  return result
+
+def x1s(R, r, a, pythag_triples, max_powers):
+  result = [-a, 0, a]
+  
+  k = R / r
+  if k.is_integer():
+    k = int(k) - 1
+    # can this be made more efficient???
+    for pa, pb, pc in pythag_triples:
+      if k > max_powers[pc]:
+        break
+      factor = k*pc**k
+      if factor > a:
+        break
+      elif a % factor == 0:
+        r1, r2 = pa*a//pc, pb*a//pc
+        result += [r1, -r1, r2, -r2]
+  elif k == 2.5 and a % 375 == 0:
+    # special case of lone 7/25
+    r = 7*a//25
+    result += [r, -r]
+  
+  return result
+
+def S(R, r, pythag_triples, max_powers):
   # x(t) = acos(t) + rcos(kt), y(t) = asin(t) - rsin(kt)
   a = R - r
   k = a / r
@@ -71,86 +119,67 @@ def S(R, r):
   # x1 = acos(t), x2 = rcos(kt), y1 = asin(t), y2 = rsin(kt)
   # iterate through values of x1, see which values of t make x2, y1, y2 integers
   
-  x1_to_check = [
-    -2, -1, 0, 1, 2,
-    -a, a,
-    -24*a/25, 24*a/25,
-    -4*a/5, 4*a/5,
-    -3*a/5, 3*a/5,
-    -7*a/25, 7*a/25,
-    -28*a/53, 28*a/53, # last might not be necessary
-    -45*a/53, 45*a/53,
-    -5*a/13, 5*a/13,
-    -12*a/13, 12*a/13,
-    -8*a/17, 8*a/17,
-    -15*a/17, 15*a/17,
-  ]
+  x1_to_check = x1s(R, r, a, pythag_triples, max_powers)
   
   lattice = set()
   
   pimuls_ = pimuls(R, r)#[b for c in range(0, 2*r+1, 2) for b in ([c] if c == 0 else [-c, c])]
   
   for x1 in x1_to_check:#[b for c in range(-a, a + 1) for b in ([c] if c == a else [c, c + 0.5])]:
-    x1half = is_half(x1)
-    if not (x1half or is_integer(x1)):
-      continue
-    
     acos_ = acos(x1/a)
     
     for pimul in pimuls_:
-      for t in (acos_, -acos_) if pimul == 0 else (acos_ + pimul*pi if pimul < 0 else pimul*pi - acos_,):
-        x2 = r*cos(k*t)
-        if not (is_half(x2) if x1half else is_integer(x2)):
+      if pimul == 0:
+        ts = (acos_, -acos_)
+      else:
+        pimuled = pimul*pi
+        ts = (acos_ + pimuled if pimul < 0 else pimuled - acos_,)
+      
+      for t in ts:
+        kt = k*t
+        
+        x2 = r*cos(kt)
+        if not is_integer(x2):
           continue
         
         y1 = a*sin(t)
-        y1half = is_half(y1)
-        if not (y1half or is_integer(y1)):
+        if not is_integer(y1):
           continue
         
-        y2 = r*sin(k*t)
-        if not (is_half(y2) if y1half else is_integer(y2)):
+        y2 = r*sin(kt)
+        if not is_integer(y2):
           continue
         
         x = round(x1 + x2)
         y = round(y1 - y2)
-        
-        if x1 not in x1_to_check:
-          print('{} not in x1_to_check!!! ({}, {}, xhalf={}, yhalf={})'.format(x1, R, r, x1half, y1half))
         
         lattice.add((x, y))
   
   return sum(abs(a) for xy in lattice for a in xy)
 
 def T(N):
+  pythag_triples = sorted(pythagorean_triples_up_to(N), key=lambda triple: triple[-1])
+  print('Pythagorean triples generated; there are {} triples: {}'.format(len(pythag_triples), pythag_triples))
+  
+  max_powers = highest_powers(pythag_triples, N)
+  
+  timestamp = clock()
+  
   total = 0
   for R in range(3, N+1):
     r_total = 0
+    
     for r in range(1, ceil(R/2)):
-      this_s = S(R, r)
+      this_s = S(R, r, pythag_triples, max_powers)
       total += this_s
       r_total += this_s
-    print(R, total, r_total)
-    sys.stdout.flush()
+    
+    if R % 100 == 0:
+      new_timestamp = clock()
+      print('R={}, total={} ({} this time), took {:.4f} seconds'.format(R, total, r_total, new_timestamp - timestamp))
+      timestamp = new_timestamp
+      
+      sys.stdout.flush()
   return total
 
-print(T(1000))
-
-# for numer in range(3, 11):
-#   for denom in range(1, ceil(numer/2)):
-#     r = 100000*denom
-#     R = numer*r//denom
-#     a = R - r
-    
-#     result = naive_c(R, r)
-    
-#     x1s = [res[3] for res in result]
-#     x2s = [res[4] for res in result]
-#     y1s = [res[5] for res in result]
-#     y2s = [res[6] for res in result]
-    
-#     print('x1 for ({}, {}, a={}): {} = {}*a'.format(R, r, a, x1s, [str(Fraction(x1, a)) for x1 in x1s]))
-#     print('x2 for ({}, {}, a={}): {} = {}*r'.format(R, r, a, x2s, [str(Fraction(x2, r)) for x2 in x2s]))
-#     print('y1 for ({}, {}, a={}): {} = {}*a'.format(R, r, a, y1s, [str(Fraction(y1, a)) for y1 in y1s]))
-#     print('y2 for ({}, {}, a={}): {} = {}*r'.format(R, r, a, y2s, [str(Fraction(y2, r)) for y2 in y2s]))
-#     print('----------------------------------------------------------')
+print(T(1000000))
