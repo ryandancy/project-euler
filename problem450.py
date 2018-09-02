68f7a7b646b2a95e1d72fe49073e4856f84a8982
Ryan Dancy <ryan@keal.ca>
2018-09-02 12:11:54 -0400

    Make problem 450 even faster, still not enough
    Problem 450's runtime has been improved even further by combining x1 and
    pimul generation into one function, which reduced redundancy and allowed
    me to remove the integer checks entirely. It's still not fast enough
    though, even though T(1000) runs in 3 seconds :(

--- problem450.py
+++ problem450.py
@@ -41,26 +41,54 @@ from itertools import count
 import numpy as np
 from time import clock
 
-def is_integer(x):
-  return abs(round(x) - x) < 0.00000001
-
-def pimuls(R, r):
+def pimuls_and_x1(R, r, a, pythag_triples, max_powers):
   n = r // gcd(R, r) if R % r else 1
   
-  if n == 1:
-    return [0]
-  elif n == 2:
-    if R % 625 == 0 and r % 250 == 0:
-      return [-2, 0, 2]
-    else:
-      return [-2, 0]
+  nmod2 = n % 2
+  nm1_divisible_4 = (n - 1) % 4 == 0
   
-  if (n - 1) % 4 == 0:
-    n0 = (-3*n - 1) // 2
-    return [n0, -n - 1, 0, -n0]
+  n0 = 0
+  
+  if n == 1:
+    n1 = 0
+    n2 = 0
+  elif n == 2:
+    n1 = 0
+    n2 = 2
+  elif nm1_divisible_4:
+    n1 = n + 1
+    n2 = (3*n + 1) // 2
   else:
-    n1 = -2*ceil(n/4)
-    return [-n - (n % 2), n1, 0, -n1]
+    n1 = 2*ceil(n/4)
+    n2 = n + nmod2
+  
+  if n % 4 == 0:
+    result = [(n0, a), (n1, a), (n2, a)]
+  elif nmod2 == 0:
+    result = [(n0, a), (n1, -a), (n2, a)]
+  elif nm1_divisible_4:
+    result = [(n0, a), (n1, -a), (n2, 0)]
+  else:
+    result = [(n0, a), (n1, 0), (n2, -a)]
+  
+  if n == 1:
+    k = R // r - 1
+    # can this be made more efficient???
+    for pa, pb, pc in pythag_triples:
+      if k > max_powers[pc]:
+        break
+      factor = k*pc**k
+      if factor > a:
+        break
+      elif a % factor == 0:
+        r1, r2 = pa*a//pc, pb*a//pc
+        result = [*result, (0, r1), (0, -r1), (0, r2), (0, -r2)]
+  elif n == 2 and R / r == 2.5 and a % 375 == 0:
+    # experiment: can "R / r == 2.5" be removed?
+    r1 = 7*a/25
+    result = [*result, (0, r1), (2, r1), (0, -r1), (2, -r1)]
+  
+  return result
 
 A = np.mat('1 -2 2; 2 -1 2; 2 -2 3')
 B = np.mat('1 2 2; 2 1 2; 2 2 3')
@@ -88,29 +116,6 @@ def highest_powers(pythag_triples, limit): # returns dict of {c: highest exponen
     result[c] = exponent
   return result
 
-def x1s(R, r, a, pythag_triples, max_powers):
-  result = [-a, 0, a]
-  
-  k = R / r
-  if k.is_integer():
-    k = int(k) - 1
-    # can this be made more efficient???
-    for pa, pb, pc in pythag_triples:
-      if k > max_powers[pc]:
-        break
-      factor = k*pc**k
-      if factor > a:
-        break
-      elif a % factor == 0:
-        r1, r2 = pa*a//pc, pb*a//pc
-        result += [r1, -r1, r2, -r2]
-  elif k == 2.5 and a % 375 == 0:
-    # special case of lone 7/25
-    r = 7*a//25
-    result += [r, -r]
-  
-  return result
-
 def S(R, r, pythag_triples, max_powers):
   # x(t) = acos(t) + rcos(kt), y(t) = asin(t) - rsin(kt)
   a = R - r
@@ -119,43 +124,27 @@ def S(R, r, pythag_triples, max_powers):
   # x1 = acos(t), x2 = rcos(kt), y1 = asin(t), y2 = rsin(kt)
   # iterate through values of x1, see which values of t make x2, y1, y2 integers
   
-  x1_to_check = x1s(R, r, a, pythag_triples, max_powers)
+  total = 0
   
-  lattice = set()
-  
-  pimuls_ = pimuls(R, r)#[b for c in range(0, 2*r+1, 2) for b in ([c] if c == 0 else [-c, c])]
-  
-  for x1 in x1_to_check:#[b for c in range(-a, a + 1) for b in ([c] if c == a else [c, c + 0.5])]:
+  for pimul, x1 in pimuls_and_x1(R, r, a, pythag_triples, max_powers):
     acos_ = acos(x1/a)
     
-    for pimul in pimuls_:
-      if pimul == 0:
-        ts = (acos_, -acos_)
-      else:
-        pimuled = pimul*pi
-        ts = (acos_ + pimuled if pimul < 0 else pimuled - acos_,)
-      
-      for t in ts:
-        kt = k*t
-        
-        x2 = r*cos(kt)
-        if not is_integer(x2):
-          continue
-        
-        y1 = a*sin(t)
-        if not is_integer(y1):
-          continue
-        
-        y2 = r*sin(kt)
-        if not is_integer(y2):
-          continue
-        
-        x = round(x1 + x2)
-        y = round(y1 - y2)
-        
-        lattice.add((x, y))
+    t = pimul*pi - acos_
+    kt = k*t
+    
+    x2 = r*cos(kt)
+    y1 = a*sin(t)    
+    y2 = r*sin(kt)
+    
+    x = round(x1 + x2)
+    y = round(y1 - y2)
+    
+    if y == 0:
+      total += abs(x)
+    else:
+      total += 2*abs(x) + 2*abs(y)
   
-  return sum(abs(a) for xy in lattice for a in xy)
+  return total
 
 def T(N):
   pythag_triples = sorted(pythagorean_triples_up_to(N), key=lambda triple: triple[-1])
@@ -164,6 +153,7 @@ def T(N):
   max_powers = highest_powers(pythag_triples, N)
   
   timestamp = clock()
+  timestamps = [timestamp]
   
   total = 0
   for R in range(3, N+1):
@@ -176,10 +166,19 @@ def T(N):
     
     if R % 100 == 0:
       new_timestamp = clock()
-      print('R={}, total={} ({} this time), took {:.4f} seconds'.format(R, total, r_total, new_timestamp - timestamp))
+      timestamps.append(new_timestamp)
+      
+      coeffs = np.polyfit(timestamps, list(range(0, R+1, 100)), 1)
+      projected = np.polyval(coeffs, N)
+      projh, projmins = divmod(projected, 3600)
+      projm, projs = divmod(projmins, 60)
+      
+      print('R={}, total={} ({} this time), took {:.4f} seconds (R={} projected to take {:.0f}h{:.0f}m{:.0f}s)'
+        .format(R, total, r_total, new_timestamp - timestamp, N, projh, projm, projs))
       timestamp = new_timestamp
       
       sys.stdout.flush()
+  
   return total
 
 print(T(1000000))
